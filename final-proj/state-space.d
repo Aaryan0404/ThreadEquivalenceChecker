530d2a5418a6d114eb1414485dda89eb31cc920b
Pranav Vaid <pranavvaid@outlook.com>
2024-03-09 17:35:55 -0800

    part 1 minus actually saving hash

---
+++ final-proj/state-space.d
@@ -0,0 +1,5 @@
+state-space.o: state-space.c sha256.h state-space.h
+
+sha256.h:
+
+state-space.h:
---
+++ temp/heythere/objs/state-space.d
@@ -0,0 +1,21 @@
+objs/state-space.o: state-space.c sha256.h \
+ /Users/pranav/Documents/cs140e-24win//libpi/include/rpi.h \
+ /Users/pranav/Documents/cs140e-24win//libpi/libc/demand.h \
+ /Users/pranav/Documents/cs140e-24win//libpi/include/gpio.h \
+ /Users/pranav/Documents/cs140e-24win//libpi/include/rpi-constants.h \
+ /Users/pranav/Documents/cs140e-24win//libpi/include/your-prototypes.h \
+ state-space.h
+
+sha256.h:
+
+/Users/pranav/Documents/cs140e-24win//libpi/include/rpi.h:
+
+/Users/pranav/Documents/cs140e-24win//libpi/libc/demand.h:
+
+/Users/pranav/Documents/cs140e-24win//libpi/include/gpio.h:
+
+/Users/pranav/Documents/cs140e-24win//libpi/include/rpi-constants.h:
+
+/Users/pranav/Documents/cs140e-24win//libpi/include/your-prototypes.h:
+
+state-space.h:
